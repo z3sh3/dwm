023855124655b1c2841d7e7591f290d7d29dd60d
zengshuai <zengs1994@gmail.com>
2026-08-07 14:31:52 +0800

    add more wmclass

--- config.def.h
+++ config.def.h
@@ -65,6 +65,7 @@ static const Rule rules[] = {
     {"jetbrains-idea",          NULL, NULL, 1 << 5, 0, -1},
     {"Code",                    NULL, NULL, 1 << 5, 0, -1},
     {"Lutris",                  NULL, NULL, 1 << 5, 0, -1},
+    {"gamescope",               NULL, NULL, 1 << 5, 0, -1},
     {"wemeetapp",               NULL, NULL, 1 << 6, 0, -1},
     {"Virt-manager",            NULL, NULL, 1 << 6, 0, -1},
     {"winboat",                 NULL, NULL, 1 << 6, 0, -1},
